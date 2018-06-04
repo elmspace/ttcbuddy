148c72a06a405ecc64a863d29bb878eb916545e1
Ash Dehghan <ash.dehghan@gmail.com>
2018-06-04 07:29:13 -0400

    Turning on the job - data collector.

--- jobs_modules/job_1/job_1.py
+++ jobs_modules/job_1/job_1.py
@@ -14,13 +14,13 @@ ttc_data_collector_obj = ttc_data_collector();
 mysql_db_obj = mysql_db();
 
 
-# for i in range(500):
-# 	data = ttc_data_collector_obj.ttc_subway_data_collector();
-# 	print(data)
-# 	mysql_db_obj.Insert_Data(data,"ttc_subway_data");
-# 	print("loading");
-# 	time.sleep(60*3);
+for i in range(500):
+	data = ttc_data_collector_obj.ttc_subway_data_collector();
+	print(data)
+	mysql_db_obj.Insert_Data(data,"ttc_subway_data");
+	print("loading");
+	time.sleep(60*10);
 
 
-data = mysql_db_obj.Select_Data("ttc_subway_data");
-print(len(data));
+# data = mysql_db_obj.Select_Data("ttc_subway_data");
+# print(len(data));
