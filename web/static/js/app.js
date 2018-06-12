801a5cdbc11e0b11dc91150df495d2a2251b1fec
Ash Dehghan <ash.dehghan@gmail.com>
2018-06-11 21:03:37 -0400

    Added HTTPS to all extrenal css elements.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,7 +1,7 @@
 // Register Service Worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('/static/service-worker.js')
     .then(function(registration) {
         console.log('Service Worker Registered');
         return registration;
